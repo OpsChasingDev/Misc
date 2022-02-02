fe8cca53e54452eda5cdae20ac2a40f06ddbcfdb
drummermanrob20 <robert@stapleton-family.net>
2022-02-02 18:06:53 -0500

    formatting and visual appeal

--- Compound-Interest.ps1
+++ Compound-Interest.ps1
@@ -15,8 +15,8 @@ $ObjectCollection = @()
 for ($i = 1; $i -le $Year; $i++) {
     $Balance = $Balance + $Contribution + ($Balance * $Growth)
     $obj = New-Object -TypeName psobject
-    $obj | Add-Member -MemberType NoteProperty -Name "Year" -Value $Year
-    $obj | Add-Member -MemberType NoteProperty -Name "Balance" -Value $Balance
+    $obj | Add-Member -MemberType NoteProperty -Name "Year" -Value $i
+    $obj | Add-Member -MemberType NoteProperty -Name "Balance" -Value $Balance.ToString('C')
     $ObjectCollection += $obj
 }
 
