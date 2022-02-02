79f068af7238d42e8da6ee456eefa275bf757653
drummermanrob20 <robert@stapleton-family.net>
2022-02-02 17:45:57 -0500

    basic structure

--- Compound-Interest.ps1
+++ Compound-Interest.ps1
@@ -1,1 +1,15 @@
-Compound-Interest.ps1
+
+param (
+    [Parameter(Mandatory)]
+    [int]$Balance,
+    [Parameter(Mandatory)]
+    [int]$Contribution,
+    [Parameter(Mandatory)]
+    [float]$Growth = 0.08,
+    [Parameter(Mandatory)]
+    [int]$Years = 1
+)
+
+for ($i = 1; $i -le $Years; $i++) {
+    $Balance = $Balance + $Contribution + ($Balance * $Growth); Write-Output $Balance
+}
