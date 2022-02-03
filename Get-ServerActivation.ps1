32f5f72261a6015916c4e464065f1b04d08a836f
drummermanrob20 <robert@stapleton-family.net>
2022-02-03 11:02:17 -0500

    take parameter multi-value

--- Get-ServerActivation.ps1
+++ Get-ServerActivation.ps1
@@ -1,5 +1,16 @@
-Get-CimInstance -ClassName SoftwareLicensingProduct |
-    Where-Object {$_.Name -like '*window*' -and
-    $_.LicenseFamily -like '*server*' -and
-    $_.LicenseStatus -eq 1} |
-    Select-Object Name,LicenseStatus,Id
+# takes a list of computer names and shows if they have activated windows server licenses
+
+param (
+    [Parameter(Mandatory)]
+    [string[]]$Computer
+)
+
+Invoke-Command -ComputerName $Computer {
+    Get-CimInstance -ClassName SoftwareLicensingProduct |
+        Where-Object {$_.Name -like '*window*' -and
+        $_.LicenseFamily -like '*server*' -and
+        $_.LicenseStatus -eq 1} |
+        Select-Object Name,LicenseStatus,Id
+} |
+Select-Object PSComputerName,Name,LicenseStatus |
+Sort-Object PSComputerName
