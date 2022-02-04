73cdf6f0c13e4ecd46bb2ca4dd6e66cd0440389d
drummermanrob20 <robert@stapleton-family.net>
2022-02-03 20:45:31 -0500

    updates and disclaimers

--- Set-vCenterEmailAlerts.ps1
+++ Set-vCenterEmailAlerts.ps1
@@ -1,48 +1,50 @@
 # this script assumes you've already connected to your vCenter instance using Connect-VIServer
 
+<#
+    CAUTION, PROCESSING PARAMETERS SUCH AS -CONFIM AND -WHATIF ARE NOT YET WRITTEN INTO THIS SCRIPT
+    THERE IS NO SAFETY NET TO PREVENT YOU FROM PERFORMING THE ACTIONS IN THIS SCRIPT IF RAN
+#>
+
 param (
     [Parameter(Mandatory)]
     [String]$email
 )
 
-# host hardware
-$HostHardware = Get-AlarmDefinition "host hardware*" | New-AlarmAction -Email -To $email -Subject "Host Hardware Problem"
-# host connection
-$HostConnection = Get-AlarmDefinition "host con*" | New-AlarmAction -Email -To $email -Subject "Host Connection Problem"
-# host cpu
-$HostCPU = Get-AlarmDefinition "host cpu*" | New-AlarmAction -Email -To $email -Subject "Host CPU Problem"
-# host memory
-$HostMemory = Get-AlarmDefinition "host memory*" | New-AlarmAction -Email -To $email -Subject "Host Memory Problem"
-# vSphere HA
-$vSphereHA = Get-AlarmDefinition "vSphere HA*" | New-AlarmAction -Email -To $email -Subject "vSphere HA Problem"
-# vCenter
-$vCenter = Get-AlarmDefinition "vCenter*" | New-AlarmAction -Email -To $email -Subject "vCenter Problem"
-# expired licenses
-$License = Get-AlarmDefinition "Expired*" | New-AlarmAction -Email -To $email -Subject "License Problem"
-# datastore space
-$DatastoreUsage = Get-AlarmDefinition "datastore usage on disk" | New-AlarmAction -Email -To $email -Subject "Datastore Usage Problem"
-# datastore connection
-$DatastoreConnection = Get-AlarmDefinition "cannot connect*" | New-AlarmAction -Email -To $email -Subject "Datastore Connection Problem"
+# the lines below each grab vCenter alarm definitions by using various string matches and stores them as variables
+# the variables are piped to the New-AlarmAction cmdlet to set the email action to each definition, categorizing them by subject
 
-<################################>
-<# remove email actions #>
-<#
 # host hardware
-$HostHardware = Get-AlarmDefinition "host hardware*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$HostHardware = Get-AlarmDefinition "host hardware*"
+$HostHardware | New-AlarmAction -Email -To $email -Subject "Host Hardware Problem"
+
 # host connection
-$HostConnection = Get-AlarmDefinition "host con*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$HostConnection = Get-AlarmDefinition "host con*"
+$HostConnection | New-AlarmAction -Email -To $email -Subject "Host Connection Problem"
+
 # host cpu
-$HostCPU = Get-AlarmDefinition "host cpu*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$HostCPU = Get-AlarmDefinition "host cpu*"
+$HostCPU | New-AlarmAction -Email -To $email -Subject "Host CPU Problem"
+
 # host memory
-$HostMemory = Get-AlarmDefinition "host memory*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$HostMemory = Get-AlarmDefinition "host memory*"
+$HostMemory | New-AlarmAction -Email -To $email -Subject "Host Memory Problem"
+
 # vSphere HA
-$vSphereHA = Get-AlarmDefinition "vSphere HA*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$vSphereHA = Get-AlarmDefinition "vSphere HA*"
+$vSphereHA | New-AlarmAction -Email -To $email -Subject "vSphere HA Problem"
+
 # vCenter
-$vCenter = Get-AlarmDefinition "vCenter*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$vCenter = Get-AlarmDefinition "vCenter*"
+$vCenter | New-AlarmAction -Email -To $email -Subject "vCenter Problem"
+
 # expired licenses
-$License = Get-AlarmDefinition "Expired*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$License = Get-AlarmDefinition "Expired*"
+$License | New-AlarmAction -Email -To $email -Subject "License Problem"
+
 # datastore space
-$DatastoreUsage = Get-AlarmDefinition "datastore usage on disk" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
+$DatastoreUsage = Get-AlarmDefinition "datastore usage on disk"
+$DatastoreUsage | New-AlarmAction -Email -To $email -Subject "Datastore Usage Problem"
+
 # datastore connection
-$DatastoreConnection = Get-AlarmDefinition "cannot connect*" | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction
-#>
+$DatastoreConnection = Get-AlarmDefinition "cannot connect*"
+$DatastoreConnection | New-AlarmAction -Email -To $email -Subject "Datastore Connection Problem"
