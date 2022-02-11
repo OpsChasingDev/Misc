5c45d5b60468bf1271c39a198256f5e3ea696562
drummermanrob20 <robert@stapleton-family.net>
2022-02-11 14:40:40 -0500

    removed comments

--- Remove-HorizonAgent.ps1
+++ Remove-HorizonAgent.ps1
@@ -6,12 +6,10 @@ $application = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\
     Where-Object {$_.DisplayName -eq 'VMware Horizon Agent'}
 $modstring = $application.ModifyPath
 
-#$split = $modstring.Split("/X")
 $split = $modstring.Split(" ")
 $split2 = $split[1]
 $split3 = $split2.Substring(2)
 
-#$fullstring = $split2 + " /forcerestart /quiet"
 $fullstring = "/X" + $split3 + " /forcerestart /quiet"
 
 Start-Process msiexec.exe -ArgumentList "$fullstring"
