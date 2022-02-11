ce0582aaa70b4ae77b55f3242a54a1c3a96b5694
drummermanrob20 <robert@stapleton-family.net>
2022-02-11 13:42:11 -0500

    Remove-HorizonAgent.ps1

---
+++ Remove-HorizonAgent.ps1
@@ -0,0 +1,10 @@
+# uninstalls the Horizon Agent from the computer
+
+$application = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, ModifyPath | Where-Object {$_.DisplayName -eq 'VMware Horizon Agent'}
+$modstring = $application.ModifyPath
+
+$split = $modstring.Split("/X")
+$split2 = $split[2]
+$fullstring = "/X" + $split2 + " /forcerestart /quiet"
+
+Start-Process msiexec.exe -ArgumentList "$fullstring"
