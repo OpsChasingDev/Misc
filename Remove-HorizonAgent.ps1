6b940e688fbcaedce606b72ecdccb07f8445421f
drummermanrob20 <robert@stapleton-family.net>
2022-02-11 14:39:05 -0500

    fixed issue with uninstall string

--- Remove-HorizonAgent.ps1
+++ Remove-HorizonAgent.ps1
@@ -1,11 +1,17 @@
 # uninstalls the Horizon Agent from the computer
 # CAUTION: THE COMPUTER WILL RESTART AUTOMATICALLY WHEN FINISHED
 
-$application = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, ModifyPath | Where-Object {$_.DisplayName -eq 'VMware Horizon Agent'}
+$application = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Select-Object DisplayName, ModifyPath |
+    Where-Object {$_.DisplayName -eq 'VMware Horizon Agent'}
 $modstring = $application.ModifyPath
 
-$split = $modstring.Split("/X")
-$split2 = $split[2]
-$fullstring = "/X" + $split2 + " /forcerestart /quiet"
+#$split = $modstring.Split("/X")
+$split = $modstring.Split(" ")
+$split2 = $split[1]
+$split3 = $split2.Substring(2)
+
+#$fullstring = $split2 + " /forcerestart /quiet"
+$fullstring = "/X" + $split3 + " /forcerestart /quiet"
 
 Start-Process msiexec.exe -ArgumentList "$fullstring"
