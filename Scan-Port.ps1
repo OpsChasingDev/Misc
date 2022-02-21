59d7e98c5f22cfcea37f5caec169a64ae499d922
drummermanrob20 <robert@stapleton-family.net>
2022-02-21 10:28:26 -0500

    Scan-Port.ps1

---
+++ Scan-Port.ps1
@@ -0,0 +1,12 @@
+$StartIP = Read-Host "Enter start IP (e.g. 45)"
+$EndIP = Read-Host "Enter end IP (e.g. 55)"
+
+$Array = @($StartIP..$EndIP)
+$Collection = @()
+$Subnet = '192.168.1.'
+
+foreach ($a in $Array) {
+    $IP = $Subnet + $a
+    # Write-Output $IP
+    $TCPTest = Test-NetConnection -ComputerName $IP -Port 443
+}
