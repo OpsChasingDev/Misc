3a6b5cd56b05f33d4795f5b16725308f99b93111
drummermanrob20 <robert@stapleton-family.net>
2022-03-18 07:06:22 -0400

    error handling

--- Get-BitLockerKey.ps1
+++ Get-BitLockerKey.ps1
@@ -1,10 +1,14 @@
 # looks for a bitlocker recovery password on the C: drive of a computer
 
-$BitLocker = Get-BitLockerVolume -MountPoint C
-$Password = $BitLocker.KeyProtector
-foreach ($p in $Password) {
-    if ($p.RecoveryPassword) {
-        $Key = $p.RecoveryPassword
-        Write-Output $Key
+$BitLocker = Get-BitLockerVolume -MountPoint C -ErrorAction SilentlyContinue
+
+if ($BitLocker) {
+    $Password = $BitLocker.KeyProtector
+    foreach ($p in $Password) {
+        if ($p.RecoveryPassword) {
+            $Key = $p.RecoveryPassword
+            Write-Output $Key
+        }
     }
 }
+else {break}
