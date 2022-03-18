2a47fae281c2a479388034fc515edd09a4a5e23a
drummermanrob20 <robert@stapleton-family.net>
2022-03-18 08:03:39 -0400

    condition to verify VolumeStatus

--- Get-BitLockerKey.ps1
+++ Get-BitLockerKey.ps1
@@ -1,6 +1,6 @@
 # looks for a bitlocker recovery password on the C: drive of a computer with the intent of passing the output to Automate to fill in a custom field
 
-$BitLocker = Get-BitLockerVolume -MountPoint C -ErrorAction SilentlyContinue
+$BitLocker = Get-BitLockerVolume -MountPoint C -ErrorAction SilentlyContinue | Where-Object {$_.VolumeStatus -eq 'FullyEncrypted'}
 
 if ($BitLocker) {
     $Password = $BitLocker.KeyProtector
