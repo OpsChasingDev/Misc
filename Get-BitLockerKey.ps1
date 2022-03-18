f4a37812a7f1099f2038e7cdf6a0e5e1f54dc910
drummermanrob20 <robert@stapleton-family.net>
2022-03-18 07:09:51 -0400

    clarified purpose comment

--- Get-BitLockerKey.ps1
+++ Get-BitLockerKey.ps1
@@ -1,4 +1,4 @@
-# looks for a bitlocker recovery password on the C: drive of a computer
+# looks for a bitlocker recovery password on the C: drive of a computer with the intent of passing the output to Automate to fill in a custom field
 
 $BitLocker = Get-BitLockerVolume -MountPoint C -ErrorAction SilentlyContinue
 
