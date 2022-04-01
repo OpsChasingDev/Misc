bd85c13b00f49c55d577489189e2334d20069871
drummermanrob20 <robert@stapleton-family.net>
2022-04-01 15:30:49 -0400

    trailing space

--- Get-VMwareHorizonAgent.ps1
+++ Get-VMwareHorizonAgent.ps1
@@ -4,4 +4,3 @@ Invoke-Command -ComputerName $VDI -ErrorAction SilentlyContinue {
     Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\AgentVersions' -Name 'View-Agent' |
     Select-Object PSComputerName, View-Agent
 }
-
