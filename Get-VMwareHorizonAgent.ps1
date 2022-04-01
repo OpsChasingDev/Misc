2ca392d83e3f8fa0ccd5ad55fd5c589199fb4305
drummermanrob20 <robert@stapleton-family.net>
2022-04-01 09:25:37 -0400

    change: multi-support

--- Get-VMwareHorizonAgent.ps1
+++ Get-VMwareHorizonAgent.ps1
@@ -1,3 +1,9 @@
+# looks up registry key on a system for the version of the VMware Horizon View Agent installed
+param (
+    [Parameter(Mandatory)]
+    [string[]]$ComputerName
+)
+
 Invoke-Command $ComputerName {
     Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\AgentVersions' -Name 'View-Agent' |
     Select-Object PSComputerName, View-Agent
