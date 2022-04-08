78c037fb16ecd4b504d3e25166a2a14596f14f9a
drummermanrob20 <robert@stapleton-family.net>
2022-04-08 15:26:04 -0400

    comment change

--- Install-HorizonAgent.8.4.ps1
+++ Install-HorizonAgent.8.4.ps1
@@ -1,4 +1,4 @@
-# install Horizon Agent 64x bit version 8.3.0-18287218
+# install Horizon Agent 64x bit version 8.4.0-19446757
 # CAUTION: THE COMPUTER WILL RESTART AUTOMATICALLY WHEN FINISHED
 
 C:\HorizonTemp\VMware-Horizon-Agent-x86_64-2111-8.4.0-19446757.exe /s /v /qn "VDM_VC_MANAGED_AGENT=1"
