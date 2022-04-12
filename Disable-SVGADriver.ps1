2a87c6561e628505390eeba74c309d6d223ef9f6
drummermanrob20 <robert@stapleton-family.net>
2022-04-12 08:35:25 -0400

    logic tested

--- Disable-SVGADriver.ps1
+++ Disable-SVGADriver.ps1
@@ -1,1 +1,15 @@
-Get-PnpDevice -FriendlyName "VMware SVGA 3D" | Disable-PnpDevice -Confirm:$false
+# checks the current installed version of the VMware Horizon Agent and disables the SVGA video driver if the Agent version is 8.4 or higher
+
+$AgentVersion = Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\AgentVersions' -Name 'View-Agent' |
+    Select-Object -ExpandProperty View-Agent
+
+$VersionNumbers = $AgentVersion.Split('.')
+[int]$NumberOne = $VersionNumbers[0]
+[int]$NumberTwo = $VersionNumbers[1]
+
+if ($NumberOne -gt 8) {
+    Get-PnpDevice -FriendlyName "VMware SVGA 3D" | Disable-PnpDevice -Confirm:$false
+}
+elseif ($NumberOne -ge 8 -and $NumberTwo -ge 4) {
+    Get-PnpDevice -FriendlyName "VMware SVGA 3D" | Disable-PnpDevice -Confirm:$false
+}
