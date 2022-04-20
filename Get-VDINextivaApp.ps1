33756c71a3990268e3eaa32a142ed9c1791775db
Robert Stapleton <89881471+drummermanrob20@users.noreply.github.com>
2022-04-20 09:58:35 -0400

    gets version and sorts by computer name

--- Get-VDINextivaApp.ps1
+++ Get-VDINextivaApp.ps1
@@ -1,3 +1,6 @@
 $VDI = (Get-AdComputer -Filter * | Where-Object {$_.Name -like '*VDI*'}).Name
 
-Invoke-Command $VDI { Get-StartApps | Where-Object name -like '*nextiva*' | Select-Object PSComputerName,Name } -ErrorAction SilentlyContinue | Sort-Object PSComputerName
+Invoke-Command $VDI {
+    Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{24D4336D-415E-4175-933F-BB9F3986484D}' -Name DisplayVersion |
+    Select-Object PSComputerName, DisplayVersion
+} -ErrorAction SilentlyContinue | Sort-Object PSComputerName
