ff74531e2c4b58a14a6b4b917bb5014a3ba94915
drummermanrob20 <robert@stapleton-family.net>
2022-04-24 10:29:15 -0400

    logic complete with example

--- Map-Range.ps1
+++ Map-Range.ps1
@@ -1,5 +1,27 @@
-$r1 = @(1..10)
-$r2 = @(10..1)
+<# PowerShell version of the algorithm for mapping one range of numbers to another range of numbers
 
-$count1 = $r1.count
-$count2 = $r2.count
+Example below to show what the output looks like for mapping a range of 0-100 to a range of 0-1
+
+PS C:\git\Misc> scale -Number 5 -inMin 0 -inMax 100 -outMin 0 -outMax 1
+0.05
+PS C:\git\Misc> scale -Number 0 -inMin 0 -inMax 100 -outMin 0 -outMax 1
+0
+PS C:\git\Misc> scale -Number 50 -inMin 0 -inMax 100 -outMin 0 -outMax 1
+0.5
+PS C:\git\Misc> scale -Number 90 -inMin 0 -inMax 100 -outMin 0 -outMax 1
+0.9
+PS C:\git\Misc>
+
+#>
+
+function scale {
+    param (
+        [int]$Number,
+        [int]$inMin,
+        [int]$inMax,
+        [int]$outMin,
+        [int]$outMax
+    )
+    $calc = ($Number - $inMin) * ($outMax - $outMin) / ($inMax - $inMin) + $outMin
+    Write-Output $calc
+}
