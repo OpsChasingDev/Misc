0baca6c6aba993cf559252683a804403a8164977
drummermanrob20 <robert@stapleton-family.net>
2022-04-28 09:42:57 -0400

    made actions in each block congruent

--- Disable-SVGADriver.ps1
+++ Disable-SVGADriver.ps1
@@ -9,11 +9,13 @@ $VersionNumbers = $AgentVersion.Split('.')
 
 if ($NumberOne -gt 8) {
     Get-PnpDevice -FriendlyName "VMware SVGA 3D" | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
-    Start-Sleep -Seconds 10
+    Start-Sleep -Seconds 2
+    Write-Output 'DISABLE'
     Restart-Computer -ComputerName $env:COMPUTERNAME -Force
 }
 elseif ($NumberOne -ge 8 -and $NumberTwo -ge 4) {
     Get-PnpDevice -FriendlyName "VMware SVGA 3D" | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
+    Start-Sleep -Seconds 2
     Write-Output 'DISABLE'
     Restart-Computer -ComputerName $env:COMPUTERNAME -Force
 }
