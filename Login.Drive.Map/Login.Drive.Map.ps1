21e9a35f0be4e2a8f21dc27bc56de42e4df4fb0d
drummermanrob20 <robert@stapleton-family.net>
2022-05-12 15:54:58 -0400

    continue login for initial drive map testing

--- Login.Drive.Map/Login.Drive.Map.ps1
+++ Login.Drive.Map/Login.Drive.Map.ps1
@@ -15,7 +15,3 @@ $letter = @(
 
 $login = cscript.exe \\SL-DC-01\NETLOGON\TestLogin.vbs
 $login = Write-Output $login[3]
-
-# determine input for shares list and drive letter mappings
-
-New-SmbShare
