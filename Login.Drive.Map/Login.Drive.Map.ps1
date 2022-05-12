4b54ebb9235b77951521a500de34a5bfa7095122
drummermanrob20 <robert@stapleton-family.net>
2022-05-12 15:38:41 -0400

    beginnings of building share and letter arrays

--- Login.Drive.Map/Login.Drive.Map.ps1
+++ Login.Drive.Map/Login.Drive.Map.ps1
@@ -1,4 +1,21 @@
+<#
+Add array here that can be manually modified to add share paths
+Each path provided will be run through upon user login
+#>
+
+$share = @(
+    # add a share name like '\\SERVER\Share'
+    '\\SL-DC-01\Tools'
+)
+
+$letter = @(
+    # add a drive letter like 'F:'
+    'T:'
+)
+
 $login = cscript.exe \\SL-DC-01\NETLOGON\TestLogin.vbs
 $login = Write-Output $login[3]
 
 # determine input for shares list and drive letter mappings
+
+New-SmbShare
