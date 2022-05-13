216cd44ba5fbf3d1751f0394b4fd2ba8dcf24631
drummermanrob20 <robert@stapleton-family.net>
2022-05-13 15:00:22 -0400

    variable changes

--- Login.Drive.Map/Login.Drive.Map.ps1
+++ Login.Drive.Map/Login.Drive.Map.ps1
@@ -1,4 +1,4 @@
-$share = @(
+$Share = @(
     '\\SL-DC-01\Tools'
     '\\SL-DC-01\TestShares'
 )
@@ -8,32 +8,33 @@ $letter = @(
     'S:'
 )
 
-$login = cscript.exe \\SL-DC-01\NETLOGON\TestLogin.vbs
-$login = Write-Output $login[3]
+$Login = cscript.exe \\SL-DC-01\NETLOGON\TestLogin.vbs
+$Login = Write-Output $Login[3]
 
 # check that number of shares and letters inputted are equal
-if ($share.count -ne $letter.count) {
+if ($Share.Count -ne $letter.Count) {
     Write-Output "The number of share and letters to map them are not equal."
     eventcreate /ID 13 /L APPLICATION /T WARNING /SO DriveMap /D "The number of share and letters to map them are not equal." > $null
     exit
 }
 
 # check that the shares have a valid UNC path
-foreach ($s in $share) {
-    if ($s -notmatch '^\\\\.') {
-        Write-Output "$s is not a valid UNC path"
-        eventcreate /ID 13 /L APPLICATION /T WARNING /SO DriveMap /D "$s is not a valid UNC path" > $null
+foreach ($h in $Share) {
+    if ($h -notmatch '^\\\\.') {
+        Write-Output "$h is not a valid UNC path"
+        eventcreate /ID 13 /L APPLICATION /T WARNING /SO DriveMap /D "$h is not a valid UNC path" > $null
         exit
     }
 }
 
 # creating objects for mapping info
+$Count = $Share.Count
 $obj = New-Object -TypeName psobject
 
 
 # mapping drives
 
-foreach ($s in $share) {
+foreach ($s in $Share) {
     New-SmbMapping -LocalPath 
 
 eventcreate /ID 13 /L APPLICATION /T WARNING /SO LOGINTEST /D "wrote successfully" > $null
