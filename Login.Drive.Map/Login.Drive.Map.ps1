c51940dfb8763a515b7e00775d12ab5f432ae0a4
drummermanrob20 <robert@stapleton-family.net>
2022-05-13 14:57:56 -0400

    built in checks for the input in both arrays

--- Login.Drive.Map/Login.Drive.Map.ps1
+++ Login.Drive.Map/Login.Drive.Map.ps1
@@ -1,21 +1,39 @@
-<#
-Add array here that can be manually modified to add share paths
-Each path provided will be run through upon user login
-#>
-
 $share = @(
-    # add a share name like '\\SERVER\Share'
     '\\SL-DC-01\Tools'
+    '\\SL-DC-01\TestShares'
 )
 
 $letter = @(
-    # add a drive letter like 'F:'
     'T:'
+    'S:'
 )
 
 $login = cscript.exe \\SL-DC-01\NETLOGON\TestLogin.vbs
 $login = Write-Output $login[3]
 
-Write-Output $login | Out-File 'C:\root\loginame.txt' -Force
+# check that number of shares and letters inputted are equal
+if ($share.count -ne $letter.count) {
+    Write-Output "The number of share and letters to map them are not equal."
+    eventcreate /ID 13 /L APPLICATION /T WARNING /SO DriveMap /D "The number of share and letters to map them are not equal." > $null
+    exit
+}
+
+# check that the shares have a valid UNC path
+foreach ($s in $share) {
+    if ($s -notmatch '^\\\\.') {
+        Write-Output "$s is not a valid UNC path"
+        eventcreate /ID 13 /L APPLICATION /T WARNING /SO DriveMap /D "$s is not a valid UNC path" > $null
+        exit
+    }
+}
+
+# creating objects for mapping info
+$obj = New-Object -TypeName psobject
+
+
+# mapping drives
+
+foreach ($s in $share) {
+    New-SmbMapping -LocalPath 
 
 eventcreate /ID 13 /L APPLICATION /T WARNING /SO LOGINTEST /D "wrote successfully" > $null
