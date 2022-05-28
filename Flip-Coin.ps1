4afa332cf31d50b22947eabc6faba770904d161c
drummermanrob20 <robert@stapleton-family.net>
2022-05-28 10:36:45 -0400

    Flip-Coin.ps1 init

---
+++ Flip-Coin.ps1
@@ -0,0 +1,21 @@
+<#
+- based off current system uptime in ticks
+- raw tick is gathered and manipulated to remove trailing zeros
+- new result is turned into a string
+- using the length of the string, separate each character
+- turn each character back into an integer
+- add the integers together
+- if the sum is even, return heads; if the sum is odd, return tails
+#>
+
+for ($i = 0; $i -lt 1000; $i++) {
+    $Collection = @()
+    $tick = ((Get-Uptime).Ticks / 10000000)
+    $t = $tick.ToString()
+    $Length = $t.Length
+    for ($l = 0; $l -lt $Length; $l++) {
+        $Index = $t.Substring(0,{$l+1})
+        $Collection += $Index
+    }
+    Start-Sleep -Seconds 1
+}
