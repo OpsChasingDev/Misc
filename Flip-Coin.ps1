c4e7df91beca212e4c5d19475d799fd3afc10db5
drummermanrob20 <robert@stapleton-family.net>
2022-06-05 09:45:23 -0400

    harvey dent

--- Flip-Coin.ps1
+++ Flip-Coin.ps1
@@ -10,19 +10,29 @@ function Flip-Coin {
         Output    
         Heads
     #>
-    
-    [int]$Sum = 0
-    $tick = ((Get-Uptime).Ticks / 10000000)
-    $t = $tick.ToString()
-    $Length = $t.Length
-    for ($l = 0; $l -lt $Length; $l++) {
-        [int]$Index = $t.Substring($l,1)
-        $Sum += $Index
+    [CmdletBinding()]
+    param (
+        [bool]$HarveyDent = $false
+    )
+
+    if ($HarveyDent -eq $false) {
+        [int]$Sum = 0
+        $tick = ((Get-Uptime).Ticks / 10000000)
+        $t = $tick.ToString()
+        $Length = $t.Length
+        for ($l = 0; $l -lt $Length; $l++) {
+            [int]$Index = $t.Substring($l,1)
+            $Sum += $Index
+        }
+        if ($Sum/2 -is [System.Double]){
+            Write-Output "Tails"
+        }
+        elseif ($Sum/2 -is [System.Int32]) {
+            Write-Output "Heads"
+        }
     }
-    if ($Sum/2 -is [System.Double]){
-        Write-Output "Tails"
-    }
-    elseif ($Sum/2 -is [System.Int32]) {
+    else {
         Write-Output "Heads"
+        Write-Host "You make your own luck..."
     }
 }
