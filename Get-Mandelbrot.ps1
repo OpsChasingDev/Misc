eee0f7532f1a7ecbc1cedf1580aea88124b71f45
drummermanrob20 <robert@stapleton-family.net>
2022-06-10 11:30:01 -0400

    injected Get-Power function

--- Get-Mandelbrot.ps1
+++ Get-Mandelbrot.ps1
@@ -1,3 +1,15 @@
+function Get-Power {
+    param (
+        [double]$StartValue,
+        [int]$PowerValue
+    )
+    $IterativeValue = $StartValue
+    for ($i = 1; $i -lt $PowerValue; $i++) {
+        $StartValue = $StartValue * $IterativeValue
+    }
+    Write-Output $StartValue
+}
+
 function Get-Mandelbrot {
     param (
         [double]$c,
