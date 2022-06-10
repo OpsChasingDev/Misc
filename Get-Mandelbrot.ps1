d21c1a5c5909a53e9fb131b1ee0ade89a81f7aa1
drummermanrob20 <robert@stapleton-family.net>
2022-06-10 08:46:20 -0400

    maybe working?

--- Get-Mandelbrot.ps1
+++ Get-Mandelbrot.ps1
@@ -2,9 +2,9 @@ function Get-Mandelbrot {
     param (
         [double]$c,
         [double]$z,
-        [int]$iteration
+        [int64]$iteration
     )
-    
+
     for ($i = 0; $i -lt $iteration; $i++) {
         $z = ($z * $z) + $c
         Write-Output $z
