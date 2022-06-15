845ef6664d31e621856b1657d9840ea26f8f82bd
drummermanrob20 <robert@stapleton-family.net>
2022-06-15 13:47:35 -0400

    main prog firewall inbound rule

--- Install-RingCentralApp.22.2.22.ps1
+++ Install-RingCentralApp.22.2.22.ps1
@@ -1,4 +1,13 @@
 # configure firewall rules
+$RCSplatInbound = @{
+    Program = 'C:\program files\ringcentral\ringcentral.exe'
+    Action = 'Allow'
+    Profile = 'Domain, Private, Public'
+    DisplayName = 'Allow RingCentral Inbound traffic'
+    Description = 'Prevents Windows Firewall from interfering with RingCentral'
+    Direction = 'Inbound'
+}
+New-NetFirewallRule @RCSplatInbound
 
 # install app
 Start-Process 'C:\Savant\RingCentral-x64.msi' -ArgumentList '/q'
