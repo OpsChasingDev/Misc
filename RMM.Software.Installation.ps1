eb8d20b1c6ec1ee78298e08a6dc12b2c689ee610
drummermanrob20 <robert@stapleton-family.net>
2022-07-27 15:42:38 -0400

    adjusted error reporting

--- RMM.Software.Installation.ps1
+++ RMM.Software.Installation.ps1
@@ -10,5 +10,5 @@ try {
 }
 catch {
     Write-Output "Installation of $Executable_File_Name on $ENV:COMPUTERNAME failed."
-    Write-Error | Out-File "C:\Savant\error.txt"
+    $Error[0].ToString() | Out-File -FilePath "C:\Savant\Error.txt"
 }
