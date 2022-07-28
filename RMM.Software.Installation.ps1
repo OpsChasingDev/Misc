e8bc9d33bfa5b2c11876abcb93b0c7f73ee0a6b6
drummermanrob20 <robert@stapleton-family.net>
2022-07-28 09:12:40 -0400

    added error file back for built-in debugging info

--- RMM.Software.Installation.ps1
+++ RMM.Software.Installation.ps1
@@ -10,4 +10,5 @@ try {
 }
 catch {
     Write-Output "Installation of $Executable_File_Name on $ENV:COMPUTERNAME failed."
+    $Error[0].ToString() | Out-File -FilePath "C:\Savant\Error.txt" -Force
 }
