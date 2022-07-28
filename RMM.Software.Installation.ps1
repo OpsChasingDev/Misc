7a795accfd5f47f0a680cf6d5cdd19b392d4786f
drummermanrob20 <robert@stapleton-family.net>
2022-07-28 09:15:25 -0400

    simplified output values for RMM capture

--- RMM.Software.Installation.ps1
+++ RMM.Software.Installation.ps1
@@ -6,9 +6,9 @@ param (
 
 try {
     Start-Process -FilePath "C:\Savant\$Executable_File_Name" -ArgumentList "$Install_Arguments_String" -Wait
-    Write-Output "Installed $Executable_File_Name on $ENV:COMPUTERNAME."
+    Write-Output "SUCCESS"
 }
 catch {
-    Write-Output "Installation of $Executable_File_Name on $ENV:COMPUTERNAME failed."
+    Write-Output "FAILED"
     $Error[0].ToString() | Out-File -FilePath "C:\Savant\Error.txt" -Force
 }
