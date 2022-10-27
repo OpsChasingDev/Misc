8de406cc2021010a4e1f8f5307fd480cc7e52099
drummermanrob20 <robert@stapleton-family.net>
2022-10-27 14:10:00 -0400

    more psuedo and notes

--- Microsoft365/Calendar.Permissions.ps1
+++ Microsoft365/Calendar.Permissions.ps1
@@ -1,16 +1,35 @@
 <#
 function {
     param (
-        [string]NewUser
+        [string]TargetUser
+        [string]Permission
         [switch]AllExisting
-        [switch]TakePerm
-        [switch]GivePerm
+        [string[]]OtherUser
+        [switch]GrantTarget
+        [switch]GrantOther
     )
 }
 
-has options:
+- sets up connection to 365 exchange using live connection
+- 
+- features:
     - add the new user to all existing mailbox calendar permissions
     - give all existing mailboxes permissions to the new user calendar
     - add the new user to specified existing mailbox calendar permissions
     - give specified existing mailboxes permissions to the new user calendar
+    - supports whatif/confirm
+- error handling:
+    - verifies connection is set up
+        - break with error if not
+    - verifies that specified user exists
+        - break with error if not
+    - verifies each change made
+        - continues if not
+        - reports if no change was made
+- input
+    - credentials to connect to 365 exchange
+    - user whose permissions need to be given/taken
+    - user(s) who will be granted access to the target user or who the target user will be granted access to
+- output
+    - confirmation information
 #>
