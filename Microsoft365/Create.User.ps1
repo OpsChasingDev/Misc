dbc7ae4d7f6c056fb5b52f96f6167868c74fbc38
drummermanrob20 <robert@stapleton-family.net>
2022-10-28 09:24:14 -0400

    changed order of setup steps

--- Microsoft365/Create.User.ps1
+++ Microsoft365/Create.User.ps1
@@ -1,12 +1,12 @@
-# prompt user input
-$FirstName = Read-Host "Enter a first name"
-$LastName = Read-Host "Enter a last name"
-$UserPass = Read-Host "Enter an initial password" -AsSecureString
-
 # connect to M365 account using Graph
 Install-Module AzureAD
 Import-Module AzureAD
 Connect-AzureAD
+
+# prompt user input
+$FirstName = Read-Host "Enter a first name"
+$LastName = Read-Host "Enter a last name"
+$UserPass = Read-Host "Enter an initial password" -AsSecureString
 
 # formulating values
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
