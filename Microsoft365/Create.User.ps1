bbded7cabf7c1dd6159a8d189ded8e5be9bbc3e4
drummermanrob20 <robert@stapleton-family.net>
2022-10-28 09:27:10 -0400

    finalized value creation and steps

--- Microsoft365/Create.User.ps1
+++ Microsoft365/Create.User.ps1
@@ -7,19 +7,22 @@ Connect-AzureAD
 $FirstName = Read-Host "Enter a first name"
 $LastName = Read-Host "Enter a last name"
 $Password = Read-Host "Enter an initial password" -AsSecureString
+$Domain = Read-Host "Enter the domain suffix for the user's UPN"
 
 # formulating values
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 $PasswordProfile.Password = $Password
+$DisplayName = $FirstName + " " + $LastName
+$UPN = $FirstName + "." + $LastName + "@" + $Domain
 
 # user creation
 $NewUserSplat = @{
-    AccountEnabled
-    DisplayName
-    GivenName
-    Surname
-    UserPrincipalName
-    PasswordProfile
+    AccountEnabled = $true
+    DisplayName = $DisplayName
+    GivenName = $FirstName
+    Surname = $FirstName
+    UserPrincipalName = $UPN
+    PasswordProfile = $PasswordProfile
 
 }
 New-AzureADUser @NewUserSplat
