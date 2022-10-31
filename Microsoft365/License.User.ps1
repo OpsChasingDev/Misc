23e2bb1ad6acaeb56be50e609e9db42d2f0e6a1b
drummermanrob20 <robert@stapleton-family.net>
2022-10-31 13:15:26 -0400

    visual format for readability

--- Microsoft365/License.User.ps1
+++ Microsoft365/License.User.ps1
@@ -8,17 +8,13 @@ $TargetUser = Read-Host "Enter the user who you want to apply licensing to (Disp
 $ExampleUser = Read-Host "Enter the example user whose licensing mirrors what you want applied to the new user (DisplayName or ObjectId)"
 
 # make sure both specified accounts exist
-try {
-    $TargetUserStored = Get-AzureADUser | Where-Object { $_.DisplayName -eq $TargetUser -or $_.ObjectId -eq $TargetUser }
-}
+try { $TargetUserStored = Get-AzureADUser | Where-Object { $_.DisplayName -eq $TargetUser -or $_.ObjectId -eq $TargetUser } }
 catch {
     Write-Error "Could not find a user account with DisplayName or ObjectID of $TargetUserStored.  The script has stopped."
     break
 }
 
-try {
-    $ExampleUserStored = Get-AzureADUser | Where-Object { $_.DisplayName -eq $ExampleUser -or $_.ObjectId -eq $ExampleUser }
-}
+try { $ExampleUserStored = Get-AzureADUser | Where-Object { $_.DisplayName -eq $ExampleUser -or $_.ObjectId -eq $ExampleUser } }
 catch {
     Write-Error "Could not find a user account with DisplayName or ObjectID of $ExampleUserStored.  The script has stopped."
     break
