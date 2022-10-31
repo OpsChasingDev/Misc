170e51e8f20bbe8c44dfca1d3a19758ca5027394
drummermanrob20 <robert@stapleton-family.net>
2022-10-31 13:12:14 -0400

    try-catch blocks to verify user accounts specified

--- Microsoft365/License.User.ps1
+++ Microsoft365/License.User.ps1
@@ -6,5 +6,18 @@
 $TargetUser = Read-Host "Enter the user who you want to apply licensing to (DisplayName or ObjectId)"
 $ExampleUser = Read-Host "Enter the example user whose licensing mirrors what you want applied to the new user (DisplayName or ObjectId)"
 
-$TargetUser = Get-AzureADUser | Where-Object { $_.DisplayName -eq $TargetUser -or $_.ObjectId -eq $TargetUser } -ErrorAction Stop
-$ExampleUser = Get-AzureADUser | Where-Object { $_.DisplayName -eq $ExampleUser -or $_.ObjectId -eq $ExampleUser } -ErrorAction Stop
+try {
+    $TargetUserStored = Get-AzureADUser | Where-Object { $_.DisplayName -eq $TargetUser -or $_.ObjectId -eq $TargetUser }
+}
+catch {
+    Write-Error "Could not find a user account with DisplayName or ObjectID of $TargetUserStored.  The script has stopped."
+    break
+}
+
+try {
+    $ExampleUserStored = Get-AzureADUser | Where-Object { $_.DisplayName -eq $ExampleUser -or $_.ObjectId -eq $ExampleUser }
+}
+catch {
+    Write-Error "Could not find a user account with DisplayName or ObjectID of $ExampleUserStored.  The script has stopped."
+    break
+}
