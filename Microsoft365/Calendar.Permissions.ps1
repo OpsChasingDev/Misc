86b7f5e3e2f6a73355998a602f98f1724620f83c
drummermanrob20 <robert@stapleton-family.net>
2022-11-01 08:31:25 -0400

    re-writing logic

--- Microsoft365/Calendar.Permissions.ps1
+++ Microsoft365/Calendar.Permissions.ps1
@@ -10,26 +10,12 @@ function {
     )
 }
 
-- sets up connection to 365 exchange using live connection
-- 
-- features:
-    - add the new user to all existing mailbox calendar permissions
-    - give all existing mailboxes permissions to the new user calendar
-    - add the new user to specified existing mailbox calendar permissions
-    - give specified existing mailboxes permissions to the new user calendar
-    - supports whatif/confirm
-- error handling:
-    - verifies connection is set up
-        - break with error if not
-    - verifies that specified user exists
-        - break with error if not
-    - verifies each change made
-        - continues if not
-        - reports if no change was made
-- input
-    - credentials to connect to 365 exchange
-    - user whose permissions need to be given/taken
-    - user(s) who will be granted access to the target user or who the target user will be granted access to
-- output
-    - confirmation information
+- set up connection to 365 exchange using the module ExchangeOnlineManagement and the Connect-ExchangeOnline cmdlet
+
+- user is prompted to choose one of the below scenarios:
+    - Identity's calendar access is being granted to all users
+    - Identity's calendar access is being granted to specified users
+    - All users' calendar access is being granted to Identity
+    - Specified users' calendar access is being granted to Identity
 #>
+
