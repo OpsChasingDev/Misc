2733532f912f82b06d104137d5721a838d642afe
drummermanrob20 <robert@stapleton-family.net>
2022-11-28 11:34:10 -0500

    starting place

--- Get-LocalAdministrators.ps1
+++ Get-LocalAdministrators.ps1
@@ -1,1 +1,11 @@
-<# in dev #>
+$path = Read-Host "Enter a full CSV file path to store information"
+
+$servers = (Get-ADComputer -Filter *).Name
+
+$RemoteOutput = Invoke-Command -ComputerName $servers -ScriptBlock {
+    $hostname = hostname
+    (Get-LocalGroupMember -Group Administrators).Name |
+        ForEach-Object { Write-Output "[$hostname] $_"}
+} -ErrorAction SilentlyContinue
+
+$RemoteOutput | Export-Csv -Path $path -NoTypeInformation
