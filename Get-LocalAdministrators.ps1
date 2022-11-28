58b466027580b34c17a38f3f2d154dce22fff6a3
drummermanrob20 <robert@stapleton-family.net>
2022-11-28 11:00:15 -0500

    added Get-LocalAdministrators.ps1

---
+++ Get-LocalAdministrators.ps1
@@ -0,0 +1,8 @@
+$servers = (Get-ADComputer -Filter * -SearchBase "DC=RubyCollins,DC=local").Name
+
+Invoke-Command -ComputerName $servers -ScriptBlock {
+    $hostname = hostname
+    $member = (Get-LocalGroupMember -Group Administrators).Name
+    $output = "[$hostname] $member"
+    Write-Output $output
+} -ErrorAction SilentlyContinue
