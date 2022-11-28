ce90be1bb6e97926e3a7840c05779e8a877bc50d
drummermanrob20 <robert@stapleton-family.net>
2022-11-28 11:33:25 -0500

    dev note

--- Get-LocalAdministrators.ps1
+++ Get-LocalAdministrators.ps1
@@ -1,7 +1,1 @@
-$servers = (Get-ADComputer -Filter *).Name
-
-Invoke-Command -ComputerName $servers -ScriptBlock {
-    $hostname = hostname
-    (Get-LocalGroupMember -Group Administrators).Name |
-        ForEach-Object { Write-Output "[$hostname] $_"}
-} -ErrorAction SilentlyContinue
+<# in dev #>
