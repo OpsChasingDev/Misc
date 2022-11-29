094931397f3d9d86aa1e9ed8ce59dc10ef736234
drummermanrob20 <robert@stapleton-family.net>
2022-11-29 11:11:57 -0500

    full logic, tested with comments

--- Get-LocalAdministrators.ps1
+++ Get-LocalAdministrators.ps1
@@ -1,6 +1,11 @@
+# prompt user for CSV file to store output; break if a valid CSV file path/name with extension is not specified
 $Path = Read-Host "Enter the full path to a CSV file where data will be stored"
+if ($Path -notmatch '.\.csv$') { break }
+
+# get all computers in the current domain
 $Computer = (Get-ADComputer -Filter *).Name
 
+# connect to each machine and return a custom PS Object containing computer names and local admin group members; output stored in CSV
 Invoke-Command -ComputerName $Computer -ScriptBlock {
     $objCol = @()
     $Member = Get-LocalGroupMember -Group Administrators
