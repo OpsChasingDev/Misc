1a60733e91b5de1678ba609bff316753103d9750
drummermanrob20 <robert@stapleton-family.net>
2022-11-29 11:13:37 -0500

    summary comments

--- Get-LocalAdministrators.ps1
+++ Get-LocalAdministrators.ps1
@@ -1,3 +1,10 @@
+<#
+    - attempts to connect to all computers in the local domain and return the members of their local Administrators group
+    - running the script first prompts the full path of a CSV file in which to store the output
+    - example CSV file full path: "C:\MyFolder\LocalAdmins.csv"
+    - if a proper CSV file full path is not specified, the script will end with no operation done
+#>
+
 # prompt user for CSV file to store output; break if a valid CSV file path/name with extension is not specified
 $Path = Read-Host "Enter the full path to a CSV file where data will be stored"
 if ($Path -notmatch '.\.csv$') { break }
